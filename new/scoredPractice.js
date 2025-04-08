5cf8ceac50377ad86d966cf5b1442517a38c1258
Allyson Wang <144262974+allysaalor@users.noreply.github.com>
2025-04-08 12:49:48 -0700

    Update scoredPractice.js

--- new/scoredPractice.js
+++ new/scoredPractice.js
@@ -3,14 +3,13 @@ const perfSpan = document.getElementById('perfect');
 const earlySpan = document.getElementById('early');
 const lateSpan = document.getElementById('late');
 const scoreSpan = document.getElementById('score');
+const hitButton = document.getElementById('hit-button');
 
 let counter = 0;
 let perfect = 0;
 let early = 0;
 let late = 0;
 let score = 0;
-
-let soundChoice = 0;
 
 hitButton.addEventListener('click', function() {
     counter++;
